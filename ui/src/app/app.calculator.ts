613148b3b475648f5d198eb63fd23ed4c24a32b2
densgorban <dens.gorban@gmail.com>
2018-07-11 12:50:15 +0300

    changes in calc ui logic

--- ui/src/app/app.calculator.ts
+++ ui/src/app/app.calculator.ts
@@ -25,11 +25,11 @@ export class AppCalculator {
   operation(type: Operation){
     if(Operation[type] === Operation.EQ) {
       var numbers = this.value.split(Operation[this.op]);
-      console.log(numbers)
+      console.log(numbers);
       this.getResult(+numbers[0], +numbers[1], this.op)
     } else if(this.op == null ){
-      this.value += Operation[type]
-      this.op = type
+      this.value += Operation[type];
+      this.op = type;
     } else console.log("Woops, you type Operation twice")
   }
 
@@ -39,21 +39,21 @@ export class AppCalculator {
   }
 
   getResult(x:number,y:number,operation:Operation){
-    //
     this._http.get(`http://localhost:8080/calculator/?x=${x}&y=${y}&opr=${operation}`
       // ,{headers: new HttpHeaders( {'Access-Control-Allow-Origin' :'http://localhost'})}
       )
       .subscribe(
         (data) => {
-          console.log(data)
-          return data;
+          console.log(data);
+          this.value = data.toString();
         }
-        // ,err => { console.log(err) }
+        ,err => {
+          console.log(err);
+          this.value = "error"
+        }
       )
   }
-
 }
-
 
 enum Operation {
   SUM = "+",
